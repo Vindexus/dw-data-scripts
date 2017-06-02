cccdf34b538ba26fefbe6d7ad2af43d7fb7e695d
Colin Kierans <kierans@monstercat.com>
2017-06-02 15:41:39 -0700

    Better output for items

--- basicmoves.js
+++ basicmoves.js
@@ -1,6 +1,12 @@
 var dw = require('dungeonworld-data')
 var console = require('./lib/console')
+var {elipses} = require('./lib/helpers');
 
 var basicData = dw.basicData
 console.h1("Basic Moves")
-basicData.basic_moves.forEach(console.move)
+basicData.basic_moves.forEach((move) => {
+  console.move(move)
+  if(move.explanation) {
+    console.label('Explanation', elipses(move.explanation, 100))
+  }
+})
--- items.js
+++ items.js
@@ -1,6 +1,10 @@
 var dw = require('dungeonworld-data')
 var console = require('./lib/console')
+var {elipses} = require('./lib/helpers');
 
+console.log('helpers', dw.helpers);
+
+console.h1('Items');
 for(var k in dw.basicData.equipment) {
   var item = dw.basicData.equipment[k]
   if(item == 'index') {
@@ -10,7 +14,14 @@ for(var k in dw.basicData.equipment) {
   console.hr2()
   console.log(item.name.bold)
   if(item.tags) {
-    console.log(item.tags.join(", ").italic)
+    //Go through all the tags on this item
+    //If it's a basic tag like 'near' then we display a snippet of the description of that tag
+    //If it's complex like {weight: 2} then we use the built in tag helper from Dungeon World Data
+    item.tags.forEach(function (givenTag) {
+      var name = dw.helpers.tag(givenTag);
+      var tag = dw.helpers.getTag(givenTag);
+      console.log(name.cyan + ': ' + elipses(tag.description, 80).gray);
+    });
   }
   if(item.description) {
     console.log(item.description)
